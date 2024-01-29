5a86debe6ade8c7abd2aead46bca1708ab35a11c
Ayan Das <ayandas9531@gmail.com>
2024-01-29 21:01:43 +0530

    Updated screenshots

--- lib/state/auth/constants.dart
+++ lib/state/auth/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
-  static String baseUrl = "https://dibook.onrender.com";
+  // static String baseUrl = "https://dibook.onrender.com";
 
-  // static String baseUrl = "http://192.168.155.218:3000";
+  static String baseUrl = "http://192.168.1.6:3000";
   static Map<String, String> contentType = {
     'Content-Type': 'application/json; charset=UTF-8'
   };
